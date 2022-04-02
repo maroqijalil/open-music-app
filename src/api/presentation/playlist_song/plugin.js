8b9a7abc6149f53c06724c017be5da6afae99efc
maroqijalil <jalilpro31@gmail.com>
2022-04-03 05:46:20 +0700

    feat: add playlist song plugin

--- src/api/presentation/playlist_song/plugin.js
+++ src/api/presentation/playlist_song/plugin.js
@@ -1,20 +1,22 @@
-import PlaylistService
-  from '../../application/services/playlist/PlaylistService.js';
-import PlaylistValidator
-  from '../../application/services/playlist/PlaylistValidator.js';
-import PlaylistRepository from
-  '../../infrastructure/repositories/PlaylistRepository.js';
+import PlaylistSongService
+  from '../../application/services/playlist_song/PlaylistSongService.js';
+import PlaylistSongValidator
+  from '../../application/services/playlist_song/PlaylistSongValidator.js';
+import PlaylistSongRepository from
+  '../../infrastructure/repositories/PlaylistSongRepository.js';
 import routes from './routes.js';
 
-const PLAYLIST_PLUGIN = {
-  name: 'playlist',
+const PLAYLIST_SONG_PLUGIN = {
+  name: 'playlist_song',
   version: '1.0.0',
   register: async (server, {database}) => {
-    const repository = new PlaylistRepository(database);
-    const service = new PlaylistService(repository, new PlaylistValidator());
+    const repository = new PlaylistSongRepository(database);
+    const service = new PlaylistSongService(
+        repository,
+        new PlaylistSongValidator());
 
     server.route(routes(service));
   },
 };
 
-export default PLAYLIST_PLUGIN;
+export default PLAYLIST_SONG_PLUGIN;
